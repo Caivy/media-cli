5ec04aa3457008c6cf1e3331ecf13a7c41de0367
Caivy <samnangkhath5@gmail.com>
2023-05-10 11:57:24 +0700

    Implement conditional git clone to check if media-cli directory already exists

--- installer.sh
+++ installer.sh
@@ -5,35 +5,62 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sudo apt-get update
   sudo apt-get install -y mpv
   sudo rm -rf "/usr/local/share/media-cli" "/usr/local/bin/media-cli" "/usr/local/bin/UI" /usr/local/bin/player_*
-  git clone "https://github.com/Caivy/media-cli.git"
+  
+  if [ -d "media-cli" ]; then
+    echo "media-cli directory already exists. Skipping git clone."
+  else
+    git clone "https://github.com/Caivy/media-cli.git"
+  fi
+  
   sudo cp media-cli/media-cli /usr/local/bin
   rm -rf media-cli
+  
   git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
   "$HOME/.fzf/install"
+  
   echo "media-cli installed successfully on Linux/MacOS!"
 
 # Install media-cli on Windows using scoop
 elif [[ "$OSTYPE" == "msys" ]]; then
   scoop bucket add extras
   scoop install mpv
-  scoop install media-cli
+  
+  if [ -d "media-cli" ]; then
+    echo "media-cli directory already exists. Skipping git clone."
+  else
+    git clone "https://github.com/Caivy/media-cli.git"
+  fi
+  
+  sudo cp media-cli/media-cli /usr/local/bin
+  rm -rf media-cli
+  
   git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
   "$HOME/.fzf/install"
+  
   echo "media-cli installed successfully on Windows!"
 
 # Install media-cli on Android using termux
 elif [[ "$OSTYPE" == "linux-android" ]]; then
   rm -rf "$PREFIX/share/media-cli" "$PREFIX/bin/media-cli" "$PREFIX/bin/UI" "$PREFIX"/local/bin/player_*
-  git clone "https://github.com/Caivy/media-cli.git"
+  
+  if [ -d "media-cli" ]; then
+    echo "media-cli directory already exists. Skipping git clone."
+  else
+    git clone "https://github.com/Caivy/media-cli.git"
+  fi
+  
   cp media-cli/media-cli "$PREFIX"/bin
   rm -rf media-cli
+  
   git clone --depth 1 "https://github.com/junegunn/fzf.git" "$HOME/.fzf"
   "$HOME/.fzf/install"
+  
   echo "media-cli installed successfully on Android!"
 
 # Install media-cli on iOS using iSH
 elif [[ "$OSTYPE" == "darwin"* && $(uname -p) == "arm" ]]; then
   apk add grep sed curl fzf git aria2 alpine-sdk ncurses
+  
   git clone https://github.com/Lockl00p/ffmpeglibs-iSH.git ~/ffmpeg
   cd ~/ffmpeg
   cat fmp.?? > ffmpeg.tar.gz
@@ -48,11 +75,11 @@ elif [[ "$OSTYPE" == "darwin"* && $(uname -p) == "arm" ]]; then
   "$HOME/.fzf/install"
   
   sudo rm -rf "/usr/local/share/media-cli" "/usr/local/bin/media-cli" "/usr/local/bin/UI" /usr/local/bin/player_*
-  git clone "https://github.com/Caivy/media-cli.git"
+  
+  if [ -d "media-cli" ]; then
+    echo "media-cli directory already exists. Skipping git clone."
+  else
+    git clone "https://github.com/Caivy/media-cli.git"
+  fi
+  
   sudo cp media-cli/media-cli /usr/local/bin
-  rm -rf media-cli
-  echo "media-cli installed successfully on iOS!"
-
-else
-  echo "Unsupported OS! media-cli installation failed."
-fi
